04604d5bd918aff0c6d228b7376a63e165c730b2
fuaad001 <husseinfuaad@hotmail.com>
2019-05-16 12:52:40 +0300

    register first models

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(tags)
