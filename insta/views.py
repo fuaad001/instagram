1250bdb242855b50efda0607e1b7208705772beb
fuaad001 <husseinfuaad@hotmail.com>
2019-05-16 12:55:57 +0300

    set up registration views

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+import datetime as dt
+from .models import
+from .forms import 
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 # Create your views here.
